b558b5d09f7355a5d53f7ff2514c4c08a7cef8f6
Hoshi Holiday <hoshiholiday@gmail.com>
2022-08-24 05:32:39 -0500

    campsitesSlice
    added campsites slice

--- 3-React/NUCAMPSITE/src/features/campsites/CampsitesList.js
+++ 3-React/NUCAMPSITE/src/features/campsites/CampsitesList.js
@@ -1,8 +1,10 @@
-import {CAMPSITES} from '../../app/shared/CAMPSITES.js';
+//import {CAMPSITES} from '../../app/shared/CAMPSITES.js';
 import {Col,Row} from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () =>{
+    const campsites = selectAllCampsites();
     return (
         <Row className="ms-auto">
              {CAMPSITES.map((campsite) => {
--- 3-React/NUCAMPSITE/src/features/campsites/campsitesSlice.js
+++ 3-React/NUCAMPSITE/src/features/campsites/campsitesSlice.js
@@ -1,4 +1,9 @@
+import CAMPSITES from "../../app/shared/CAMPSITES";
+
 export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
+export const selectRandomCampsite =()=>{
+    return CAMPSITES[Math.floor(CAMPSITES.length*Math.random())];
+}
