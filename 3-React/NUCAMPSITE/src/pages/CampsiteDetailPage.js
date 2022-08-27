015c005e47b279c56d79a4ecca1dac3891cbcd6a
Hoshi Holiday <hoshiholiday@gmail.com>
2022-08-27 05:57:34 -0500

    "Breadcrumbs SubHeader"
    learned how to integrate conditional logic within the referenced Subheader component across files (CampsitesDirectory,& HomePage) and use props to declare the breadcrumbs.

--- 3-React/NUCAMPSITE/src/pages/CampsiteDetailPage.js
+++ 3-React/NUCAMPSITE/src/pages/CampsiteDetailPage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CommentsList from '../features/comments/CommentsList';
+import SubHeader from '../components/SubHeader';
 
 const CampsiteDetailPage = () => {
     const { campsiteId } = useParams();
@@ -11,6 +12,7 @@ const CampsiteDetailPage = () => {
     return (
         <Container>
             <Row>
+                <SubHeader current={campsite.name} detail={true} />
                 <CampsiteDetail campsite={campsite} />
                 <CommentsList campsiteId={campsiteId} />
             </Row>
--- 3-React/NUCAMPSITE/src/pages/CampsitesDirectoryPage.js
+++ 3-React/NUCAMPSITE/src/pages/CampsitesDirectoryPage.js
@@ -1,10 +1,14 @@
-import { Container } from 'reactstrap';
+import { Container,Row } from 'reactstrap';
+import SubHeader from '../components/SubHeader';
 import CampsitesList from '../features/campsites/CampsitesList';
 
 const CampsitesDirectoryPage = () => {
     return (
         <Container>
+            <Row>
+            <SubHeader current={'Directory'}/>
             <CampsitesList />
+            </Row>
         </Container>
     );
 };
