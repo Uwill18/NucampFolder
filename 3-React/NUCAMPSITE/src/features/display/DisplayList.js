3e40625b1254badfdc381b378881d7c3c90c481a
Hoshi Holiday <hoshiholiday@gmail.com>
2022-08-31 16:59:26 -0500

    AnimatedDisplayCard
    setup the animated display card

--- 3-React/NUCAMPSITE/src/features/display/DisplayList.js
+++ 3-React/NUCAMPSITE/src/features/display/DisplayList.js
@@ -1,24 +1,21 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+//import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
+import {AnimatedDisplayCard}  from './AnimatedDisplayCard';
 
 
 const DisplayList = () => {
     const items = [selectFeaturedCampsite(), selectFeaturedPromotion(),selectFeaturedPartner()];
-
     return (
-        <Row>
-            {items.map((item, idx) => {
-                return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
-                );
-            })}
-        </Row>
+        item && (
+            <Col md className='m-1' key={idx}>
+                <AnimatedDisplayCard item={item} />
+            </Col>
+        )
     );
+
 };
 
 export default DisplayList;
