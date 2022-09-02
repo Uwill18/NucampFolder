006cd2a7c8c4998baff8d1abf988e46719e43e30
Hoshi Holiday <hoshiholiday@gmail.com>
2022-09-02 16:41:36 -0500

    contact form part 1
    part 1 of formik

--- 3-React/NUCAMPSITE/src/pages/ContactPage.js
+++ 3-React/NUCAMPSITE/src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import { Container, Col, Row } from 'reactstrap';
 import SubHeader from '../components/SubHeader';
+import ContactForm from '../components/ContactForm';
 
 const ContactPage = () => {
     return (
@@ -41,7 +42,9 @@ const ContactPage = () => {
                     <h2>Send Us Your Feedback</h2>
                     <hr />
                 </Col>
-                <Col md='10'>TBD: ContactForm</Col>
+                <Col md='10'>
+                    <ContactForm />
+                </Col>
             </Row>
         </Container>
     );
