03dc3d7efac18ef16d14507eb175863894b61586
Hoshi Holiday <hoshiholiday@gmail.com>
2022-09-02 17:03:48 -0500

    task 3 done
    finished task 3/6 of formik

--- 3-React/NUCAMPSITE/src/components/ContactForm.js
+++ 3-React/NUCAMPSITE/src/components/ContactForm.js
@@ -17,6 +17,7 @@ const ContactForm = () =>{
                         First Name
                     </Label>
                     <Col md='10'>
+                        <Field name='firstName' placeholder='FirstName'/>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -24,6 +25,7 @@ const ContactForm = () =>{
                         Last Name
                     </Label>
                     <Col md='10'>
+                    <Field name='lastName' placeholder='LastName'/>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -31,6 +33,7 @@ const ContactForm = () =>{
                         Phone
                     </Label>
                     <Col md='10'>
+                    <Field name='phoneNum' placeholder='Phone Number'/>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -38,13 +41,23 @@ const ContactForm = () =>{
                         Email
                     </Label>
                     <Col md='10'>
+                    <Field name='email' placeholder='Email'/>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                 <Label check md={{ size: 4, offset: 2 }}>
+                        <Field
+                            name='agree'
+                            type='checkbox'
+                            className='form-check-input'
+                        />{' '}
                         May we contact you?
                     </Label>
                     <Col md='4'>
+                    <Field name='contactType' as='select'>
+                    <option>By Phone</option>
+                    <option>By Email</option>
+                    </Field>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -52,6 +65,7 @@ const ContactForm = () =>{
                         Your Feedback
                     </Label>
                     <Col md='10'>
+                        <Field name='feedback' as='textarea' rows='12'/>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
