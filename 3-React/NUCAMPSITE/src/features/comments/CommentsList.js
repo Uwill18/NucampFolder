2c92e38851657e25f6929a7040d3f82107a119be
Hoshi Holiday <hoshiholiday@gmail.com>
2022-09-03 12:36:29 -0500

    Workshop 3
    Created a clickable modal that validates user info

--- 3-React/NUCAMPSITE/src/features/comments/CommentsList.js
+++ 3-React/NUCAMPSITE/src/features/comments/CommentsList.js
@@ -1,6 +1,7 @@
 import { Col } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
+import CommentForm from './CommentForm';
 
 const CommentsList = ({ campsiteId }) => {
     const comments = selectCommentsByCampsiteId(campsiteId);
@@ -12,6 +13,7 @@ const CommentsList = ({ campsiteId }) => {
                 {comments.map((comment) => {
                     return <Comment key={comment.id} comment={comment} />;
                 })}
+                <CommentForm campsiteId={campsiteId}/>
             </Col>
         );
     }
