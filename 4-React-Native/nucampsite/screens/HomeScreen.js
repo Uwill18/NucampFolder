c60cd2311082612e5d0af8f200bd428d794bff06
Hoshi Holiday <hoshiholiday@gmail.com>
2022-09-30 12:01:19 -0500

    Navigation Part 3
    Set up the Home screen to display the three featured items from the campsites, promotions, and partners data files.

--- 4-React-Native/nucampsite/screens/HomeScreen.js
+++ 4-React-Native/nucampsite/screens/HomeScreen.js
@@ -1,12 +1,50 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
 
+const FeaturedItem = ({ item }) => {
+    if (item) {
+        return (
+            <Card containerStyle={{ padding: 0 }}>
+                <Card.Image source={item.image}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
+                            }}
+                        >
+                            {item.name}
+                        </Text>
+                    </View>
+                </Card.Image>
+                <Text style={{ margin: 20 }}>{item.description}</Text>
+            </Card>
+        );
+    }
+    return <View />;
+};
 
 const HomeScreen = () => {
-  return (
-    <View>
-    <Text>Home Screen</Text>
-</View>
-  )
-}
+    const [campsites, setCampsites] = useState(CAMPSITES);
+    const [promotions, setPromotions] = useState(PROMOTIONS);
+    const [partners, setPartners] = useState(PARTNERS);
+
+    const featCampsite = campsites.find((item) => item.featured);
+    const featPromotion = promotions.find((item) => item.featured);
+    const featPartner = partners.find((item) => item.featured);
+
+    return (
+        <ScrollView>
+            <FeaturedItem item={featCampsite} />
+            <FeaturedItem item={featPromotion} />
+            <FeaturedItem item={featPartner} />
+        </ScrollView>
+    );
+};
 
 export default HomeScreen;
